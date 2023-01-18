1b3812318487b62126f1c7a114f99be52f3e86f5
Arthur <92685845+Artur420Art@users.noreply.github.com>
2023-01-18 13:57:56 +0400

    Update ceasar.h

--- ceasar.h
+++ ceasar.h
@@ -7,13 +7,12 @@ class Ceasar
 {
 public:
     Ceasar(std::string&, std::string&, int);
-    // ~Ceasar();
+    ~Ceasar();
     void init();
     void fetch(std::string str);
     std::string m_decode(std::string& str, const size_t pos);
     std::string m_encode(std::string& str, const size_t pos);
 private:
-//  typedef std::string(Ceasar::*ptr) (std::string& str, const size_t pos);
     using ptr = std::string(Ceasar::*) (std::string& , const size_t );
 private:
 
